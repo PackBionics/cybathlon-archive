3b43a0c7904bda061541d3b99c442550b3c1207a
awkyu <awkyu@ncsu.edu>
2019-12-04 20:33:14 -0500

    added motor function

--- Master Sketch/Master/Motor.h
+++ Master Sketch/Master/Motor.h
@@ -6,5 +6,12 @@
 #define PWM         9                     //Pin for Power of motor.
 #define DIR         10                    //Pin for Direction of the motor.
 
+#define STOP_DIST   5                     // error for stopping motor
+#define MAX_DIST    16384                 // 2^14 is the max distance
+#define MOTOR_PWR   200                   // max motor power = MOTOR_PWR + MIN_MPWR <= 255
+#define MIN_MPWR    55                    // minimum motor power
+
 // functions
 void Init_Motors(void);
+uint16_t rotate(uint16_t halt, uint8_t pwm, uint8_t dir);
+uint16_t stall(uint8_t pwm, uint8_t dir);
--- Master Sketch/Master/Motor_functions.ino
+++ Master Sketch/Master/Motor_functions.ino
@@ -5,3 +5,37 @@ void Init_Motors()
   analogWrite(PWM,0);
   digitalWrite(DIR,HIGH);
 }
+
+// we are assuming high for dir is clockwise
+/**
+ * @param halt is the stopping position
+ * @param is the max speed
+ * @param dir is the direction
+ * @return the current position
+ */
+uint16_t rotate(uint16_t halt, uint8_t pwm, uint8_t dir)
+{
+  int curr_pos = getPositionSPI(ENC_0, RES14);
+  int change_dist = dir ? halt - curr_pos : MAX_DIST - (halt - curr_pos);
+  int init_dist = change_dist;
+
+  while (abs(change_dist) > STOP_DIST)
+  {
+    // calculate speed ratio (ramps up and down; fastest at half the distance travelled)
+    int curr_speed = 1 - abs(change_dist - init_dist / 2);
+    int speedM = curr_speed * MOTOR_PWR + MIN_MPWR;
+    // write to the motor to go the desired speed in the desired direction
+    digitalWrite(DIR, dir);
+    analogWrite(PWM, speedM);
+
+    // get new current position
+    curr_pos = getPositionSPI(ENC_0, RES14);
+    change_dist = dir ? halt - curr_pos : MAX_DIST - (halt - curr_pos);
+  }
+  return curr_pos;
+}
+
+uint16_t stall(uint8_t pwm, uint8_t dir)
+{
+  return 0;
+}
