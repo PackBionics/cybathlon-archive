0d3abe119d7e7d7cf89466a4e8bf1808d72dcfd7
Emily Keller <lady.buggy@hotmail.com>
2020-03-09 21:12:05 -0400

    Updated Diagnose file

--- Diagnostic_Protocol/Diagnose/Diagnose.ino
+++ Diagnostic_Protocol/Diagnose/Diagnose.ino
@@ -53,8 +53,8 @@ void loop() {
   if (startDialogue) {
     Serial.println("Welcome to the the Stryder Diagnostic Protocol! Select one of the options below to diagnose.");
     Serial.println("1: Motor and Absolute Encoders");
-    Serial.println("2: Load Cells");
-    Serial.println("3: Accelerometer");
+    Serial.println("2: Accelerometer");
+    Serial.println("3: Load Cells");
     Serial.println("4: Lock Button");
     startDialogue = false;
     dialogue1 = true;
@@ -69,7 +69,7 @@ void loop() {
     switch (input) {
         case 1:
           if (dialogue1) {
-            Serial.println("Please type in desired knee angle. Type ""Q"" to quit Motor and Absolute Encoders Diagnosis.");
+            Serial.println("Please type in desired knee angle. Type Q to quit Motor and Absolute Encoders Diagnosis.");
             dialogue1 = false;
           } else {
             if (Serial.available() > 0) {
@@ -93,15 +93,50 @@ void loop() {
           break;
           
         case 2:
-
+          if (dialogue1) {
+            Serial.println("Type Q to quit Accelerometer Diagnosis.");
+            Serial.println("Please specify which axis you are testing.");
+            Serial.println("Y : Leg is straight up and down.");
+            Serial.println("X : Leg is straight out infront");
+            Serial.println("Z : Leg is straight out to the side.");
+            dialogue1 = false;
+          } else {
+            if (Serial.available() > 0) {
+              String input = Serial.readString();
+              if (input.equals("Q")) {
+                startDialogue = true;
+                input = 0;
+                Serial.println("Quiting");
+              } else if (input.equals("Y")) {
+                Serial.println("Accelerometer Reading: " + getY() + " G's");                
+              } else if (input.equals("X")) {
+                Serial.println("Accelerometer Reading: " + getX() + " G's"); 
+              } else if (input.equals("Z")) {
+                Serial.println("Accelerometer Reading: " + getZ() + " G's"); 
+              } else {
+                Serial.println("Invalid Input");
+              }
+            }
+          }
           break;
           
         case 3:
-
+          Serial.println("Front: " + lcFront + "N Back: " + lcBack + "N Left: " + lcLeft + "N Right: " + lcRight + "N");
           break;
           
         case 4:
-
+          Serial.println("Begin with the leg unlocked");
+          delay(2000);
+          if (button_state() > 0) {
+            Serial.println("Please unlock leg");
+          } else {
+            Serial.println("Now lock the leg");
+            if (button_state() > 0) {
+              Serial.println("Button is pushed");
+            } else {
+              Serial.println("");
+            }
+          }
           break;
       }
   } 
