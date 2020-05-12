4d9c163e5749252320410977e6f803710f1dae1d
unknown <amkyu@ncsu.edu>
2020-05-11 20:23:10 -0400

    Modified inputs to account for more scenarios

--- Black_Box _Tests/Motor_Control_Test2/Motor_Control_Test2.ino
+++ Black_Box _Tests/Motor_Control_Test2/Motor_Control_Test2.ino
@@ -8,6 +8,8 @@
 #include "Inner_FSMs.h"
 #include "Accelerometer_functions.h"
 
+#include <string.h>
+#include <stdio.h>
 
 //#define DEBUG_CONFIG // comment this line out to run full system
 //#define CALIBRATION_CONFIG_LC // comment this line out to run full system
@@ -16,8 +18,16 @@
 
 long tic;
 long toc;
+int startingPos;
 int firstPos;
+int triggerAngle;
 int secondPos;
+boolean startTest;
+
+char seps[] = ",";
+char *token;
+int var;
+int input[4];
   
 void setup() {
   // Initialize
@@ -28,6 +38,7 @@ void setup() {
   Init_Interrupt();
   Init_Accelerometer();
   Serial.begin(9600);
+  startTest = false;
 
 
 #ifdef DEBUG_CONFIG
@@ -56,24 +67,50 @@ void setup() {
 }
 
 void loop() {
-  
+  // input order: starting position, first position, trigger angle, second position
   if (Serial.available() > 0) {
     String s = Serial.readString();
-    int s0 = s.indexOf(",");
-    String s1 = s.substring(0,s0);
-    String s2 = s.substring(s0+1);
-    firstPos = s1.toInt();
-    secondPos = s2.toInt();
-    Serial.print(firstPos);
-    Serial.print("\t");
-    Serial.println(secondPos);
-    rotate(firstPos);
+    token = strtok(s, seps);
+    for (int i = 0; i < 4; i++) {
+      sscanf(token, "%d", &var);
+      input[i] = var;
+      token = strtok(NULL, seps);
+    }
+    startPos = input[0];
+    firstPos = input[1];
+    triggerAngle = input[2];
+    secondPos = input[3];
+    startTest = true;
     tic = millis();
   }
-  toc = millis();
-  if (toc-tic > 500) {
-    rotate(secondPos);
+  if (startTest) {
+    rotate(startPos);
+    toc = millis();
+    if (toc-tic > 1000) {
+      rotate(firstPos);
+    }
+    if (toc-tic > 1000 && encKnee == triggerAngle) {
+      rotate(secondPos);
+      startTest = false;
+    }
   }
+//  if (Serial.available() > 0) {
+//    String s = Serial.readString();
+//    int s0 = s.indexOf(",");
+//    String s1 = s.substring(0,s0);
+//    String s2 = s.substring(s0+1);
+//    firstPos = s1.toInt();
+//    secondPos = s2.toInt();
+//    Serial.print(firstPos);
+//    Serial.print("\t");
+//    Serial.println(secondPos);
+//    rotate(firstPos);
+//    tic = millis();
+//  }
+//  toc = millis();
+//  if (toc-tic > 500) {
+//    rotate(secondPos);
+//  }
 //  Serial.print("Knee Encoder: ");
 //  Serial.print(encKnee);
 //  Serial.print("\t");
